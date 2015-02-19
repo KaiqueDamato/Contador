45549b35e9ca10e0789eae542401f554384e6910
Kaique Damato <kaique_pantosi@hotmail.com>
2015-02-19 08:53:32 -0200

    mudei as linhas de comentário

--- ContaPessoas/FirstViewController.m
+++ ContaPessoas/FirstViewController.m
@@ -2,8 +2,8 @@
 //  FirstViewController.m
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Kaique Pantosi on 2/18/15.
+//  Copyright (c) 2015 Kaique Pantosi. All rights reserved.
 //
 
 #import "FirstViewController.h"
